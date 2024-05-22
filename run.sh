095b2790b2daa1b5b1b7060691e1c7ee0b69b8c4
通晓宇宙 <2581011320@qq.com>
2024-05-23 01:11:24 +0800

    配置相关

--- run.sh
+++ run.sh
@@ -32,7 +32,42 @@ cd ..
 docker-compose build
 
 # 2 初始化
-# TODO
+# user
+if [ ! -d ./config/user ]; then
+    mkdir -p ./config/user
+    echo "下面是微信相关配置，参考: "
+    echo "https://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index"
+
+    read -p "请输入微信公众号的AppID: " appid
+    read -p "请输入微信公众号的AppSecret: " appsecret
+
+    cat <<EOF >./config/user/application.yml
+nwu:
+  wechat:
+    appid: $appid
+    secret: $appsecret
+EOF
+fi
+
+# system
+if [ ! -d ./config/system ]; then
+    mkdir -p ./config/system
+    echo "下面是阿里云相关配置，参考: "
+    echo "https://ram.console.aliyun.com/manage/ak"
+
+    read -p "请输入阿里云的AccessKey ID: " accesskeyid
+    read -p "请输入阿里云的AccessKey Secret: " accesskeysecret
+
+    cat <<EOF >./config/system/application.yml
+aliyun:
+  oss:
+    access-key-id: $accesskeyid
+    access-key-secret: $accesskeysecret
+    endpoint: https://oss-cn-hangzhou.aliyuncs.com
+    bucket-name: online-hospital-nwu
+EOF
+fi
+
 echo "微服务已配置完成！"
 
 # 3 启动

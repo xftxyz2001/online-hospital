1da6f60c3a068dc855e73214ec2ebb45a033d5a3
通晓宇宙 <2581011320@qq.com>
2024-05-22 21:47:26 +0800

    登出

--- site-doctor/src/components/aside/index.vue
+++ site-doctor/src/components/aside/index.vue
@@ -4,7 +4,7 @@
       active-text-color="#ffd04b"
       background-color="#324157"
       class="el-menu-vertical-demo"
-      default-active="2"
+      default-active="1"
       text-color="#fff"
       style="height: 700px"
     >
--- site-doctor/src/components/top/index.vue
+++ site-doctor/src/components/top/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
     <div class="left" style="color: white; font-size: 17px; display: flex; align-items: center">
-      <img src="../../../src/assets/接诊.png" alt="" width="30px" />
+      <img src="@/assets/接诊.png" alt="" width="30px" />
       医生问诊
     </div>
-    <div class="right">退出</div>
+    <div class="right" @click="logout" style="cursor: pointer;">退出</div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+const logout = () => {
+  router.push({
+    path: "/login"
+  });
+};
 </script>
 
 <style>

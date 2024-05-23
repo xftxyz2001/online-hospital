d1ae60ad62f97c90d0c07aaf2f59b06f4443da20
通晓宇宙 <2581011320@qq.com>
2024-05-23 10:37:01 +0800

    修复首次登录页面内容不加载

--- site-admin/src/utils/request.js
+++ site-admin/src/utils/request.js
@@ -3,7 +3,7 @@
 //响应拦截器可以简化返回的数据
 import axios from "axios";
 import store from "@/vuex";
-const useToken = store.getters.managerLoginToken;
+
 //二次封装，设置一些参数
 const request = axios.create({
   baseURL: "/", //设置基础路径
@@ -11,43 +11,20 @@ const request = axios.create({
 });
 //设置请求拦截器
 request.interceptors.request.use(config => {
-  const userToken = store.getters.managerLoginToken;
+  const token = store.getters.managerLoginToken;
 
-  if (useToken != "") {
+  if (token != "") {
     config.headers = {
-      token: userToken
+      token
     };
   }
-  // config => {
-  //     //
-  //     console.log(2);
-
-  //     //console.log("请求头toekn=====>", useToken.token);
-  //     // 设置请求头
-  //     // config.headers['token'] = useToken.token;
-  //     //   config.headers.token = useToken;
-  // };
-  // }
-
   return config;
 });
+
 //设置响应拦截器
 request.interceptors.response.use(
   response => {
     //响应成功
-    // let code = response.data.code;
-    // console.log("code="+code);
-
-    // switch(code)
-    // {
-    //     //请求成功
-    //     case 1:
-    //         console.log(response.data.data);
-    //         console.log("intercepter  "+new Date().getTime());
-
-    //         return response.data.data;
-    //         break;
-    // }
     return response.data;
   },
   error => {
